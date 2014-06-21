a08c9591039e60ac3ff93702cf394609ce31dde6
Chehab Mustafa-Hilmy <m@chehab.me>
2014-06-21 19:32:22 +0200

    [FIX] a work-around for setting vars. on the fly using env. variables. Refer to the read me for more information.

--- settings.py
+++ settings.py
@@ -25,8 +25,23 @@ PORT = 8080
 ## Your own settings here
 LANG='en' # Language Setting values [ar|en]
 
-#############################
-## THIS IS A HACK FOR SCSS ##
+
+## THIS IS A HACK ##
+################################################################################
+## Settings from env variables ##
+import os
+try:
+  LANG_t = os.environ['CLAY_LANG']
+  if LANG_t.lower() in ['en', 'ar']:
+    LANG = LANG_t.lower()
+  else:
+    print ">>> <X> ERROR CLAY_LANG not defined correctly, value must be [en|ar]"
+    print "          Using Settings Defualt ({})".format(LANG)
+except Exception as e:
+  print ">>> <!> CLAY_LANG not defined, Using Settings Defualt ({})".format(LANG)
+
+################################################################################
+## SCSSS                                                                      ##
 import sys
 import subprocess
 if 'run' in sys.argv:
@@ -34,4 +49,6 @@ if 'run' in sys.argv:
   subprocess.call(['screen','-d','-m','sass','--watch','source/assets/stylesheets:source/assets/stylesheets'])
 elif 'build' in sys.argv:
   print "SASS Build"
-  subprocess.call(['sass','source/assets/stylesheets/fabstrap.scss','source/assets/stylesheets/fabstrap.css'])
+  scss_file = 'source/assets/stylesheets/fabstrap-{}.scss'.format(LANG)
+  css_file  = 'source/assets/stylesheets/fabstrap-{}.css'.format(LANG)
+  subprocess.call(['sass',scss_file,css_file])
