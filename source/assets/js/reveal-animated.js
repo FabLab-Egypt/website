1636a9d580b9c2b5c7d92cf02e500e10889ec9ac
Chehab Mustafa-Hilmy <m@chehab.me>
2014-06-23 02:40:44 +0200

    refactored, added flipIn_ animation.

--- source/assets/js/reveal-animated.js
+++ source/assets/js/reveal-animated.js
@@ -1,9 +1,23 @@
+
+/*******************************************************************************
+
+             d888                888         8888888
+            d88P"                888           888
+            888                  888           888
+            888888  8888b.   .d88888  .d88b.   888   88888b.
+            888        "88b d88" 888 d8P  Y8b  888   888 "88b
+            888    .d888888 888  888 88888888  888   888  888
+            888    888  888 Y88b 888 Y8b.      888   888  888
+            888    "Y888888  "Y88888  "Y8888 8888888 888  888
+
+*******************************************************************************/
+
+
 function reveal_animate_fadeIn_(selector)
 {
   reveal_animate_fadeInLeft(selector);
   reveal_animate_fadeInRight(selector);
 }
-
 
 function reveal_animate_fadeInLeft(selector, remove_animation_class)
 {
@@ -17,12 +31,11 @@ function reveal_animate_fadeInLeft(selector, remove_animation_class)
             if (remove_animation_class){
               $(this)
                 .removeClass('animated')
-                  .removeClass('fadeInRight');
+                  .removeClass('fadeInLeft');
             }
           }
         );
 }
-
 
 function reveal_animate_fadeInRight(selector, remove_animation_class)
 {
@@ -41,6 +54,57 @@ function reveal_animate_fadeInRight(selector, remove_animation_class)
           }
         );
 }
+
+function reveal_animate_fadeInUp(selector, remove_animation_class)
+{
+  selector = selector+'.reveal-fadeInUp'
+  $(selector)
+    .removeClass('invisible')
+      .addClass('animated fadeInUp')
+        .one(
+          'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+          function()
+          {
+            if (remove_animation_class){
+              $(this)
+                .removeClass('animated')
+                  .removeClass('fadeInUp');
+            }
+          }
+        );
+}
+
+function reveal_animate_fadeInDown(selector, remove_animation_class)
+{
+  console.log("reveal_animate_fadeInDown("+selector+'.reveal-fadeInDown)');
+  $(selector+'.reveal-fadeInDown')
+    .removeClass('invisible')
+      .addClass('animated fadeInDown')
+        .one(
+          'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+          function()
+          {
+            if (remove_animation_class){
+              $(this)
+                .removeClass('animated')
+                  .removeClass('fadeInDown');
+            }
+          }
+        );
+}
+
+/*******************************************************************************
+
+         .d888               888           .d88888b.           888
+        d88P"                888          d88P" "Y88b          888
+        888                  888          888     888          888
+        888888  8888b.   .d88888  .d88b.  888     888 888  888 888888
+        888        "88b d88" 888 d8P  Y8b 888     888 888  888 888
+        888    .d888888 888  888 88888888 888     888 888  888 888
+        888    888  888 Y88b 888 Y8b.     Y88b. .d88P Y88b 888 Y88b.
+        888    "Y888888  "Y88888  "Y8888   "Y88888P"   "Y88888  "Y888
+
+*******************************************************************************/
 
 function reveal_animate_fadeOut_(selector)
 {
@@ -76,4 +140,62 @@ function reveal_animate_fadeOutRight(selector)
               .removeClass('fadeOutRight');
         }
       );
+}
+
+
+
+
+/*******************************************************************************
+
+                   .d888 888 d8b         8888888
+                  d88P"  888 Y8P           888
+                  888    888               888
+                  888888 888 888 88888b.   888   88888b.
+                  888    888 888 888 "88b  888   888 "88b
+                  888    888 888 888  888  888   888  888
+                  888    888 888 888 d88P  888   888  888
+                  888    888 888 88888P" 8888888 888  888
+                                 888
+                                 888
+                                 888
+
+*******************************************************************************/
+
+
+function reveal_animate_flipInX(selector, remove_animation_class)
+{
+  selector = selector+'.reveal-flipInX'
+  $(selector)
+    .removeClass('invisible')
+      .addClass('animated flipInX')
+        .one(
+          'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+          function()
+          {
+            if (remove_animation_class){
+              $(this)
+                .removeClass('animated')
+                  .removeClass('flipInX');
+            }
+          }
+        );
+}
+
+function reveal_animate_flipInY(selector, remove_animation_class)
+{
+  selector = selector+'.reveal-flipInY'
+  $(selector)
+    .removeClass('invisible')
+      .addClass('animated flipInY')
+        .one(
+          'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+          function()
+          {
+            if (remove_animation_class){
+              $(this)
+                .removeClass('animated')
+                  .removeClass('flipInY');
+            }
+          }
+        );
 }
