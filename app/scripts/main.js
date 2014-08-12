e6841520e2b6736443c827b117ce7549e3257f9c
Chehab Mustafa-Hilmy <m@chehab.me>
2014-08-12 20:40:37 +0300

    [FIX] contact us section map reimplemented using GMap.JS, fixes #3 Moved map javascript to separate file 'gmap.js' clearing 'main.js'. contact section background-color changed to match previous section (membership).
    [ADDED] bootstrap 3.x via bower for javascript.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,9 +2,6 @@
 
 /* global revealAnimate: true */
 /* global Pace: true */
-/* global google: true */
-/* global map: true */
-/* global marker: true */
 
 // PaceJS: As Pace Complete
 Pace.once('done', function(){
@@ -91,48 +88,10 @@ $(function()
     );
 });
 
+
+
 /******************************************************/
-/******************************************************/
-/** Contact Section Map                              **/
-
-$(function() {
-
-    // Snippt source: http://www.labnol.org/internet/embed-google-maps-background/28457/
-    // Similar Snippt: http://sickdesigner.com/fun-with-google-maps
-
-    console.log('MAP-SECTION');
-    var position = [30.049019020085613, 31.20098772209326];
-    var fpoint = [30.049007411520297, 31.202280546838438];
-
-    function showGoogleMaps() {
-
-        var latLng = new google.maps.LatLng(position[0], position[1]);
-        var focuse = new google.maps.LatLng(fpoint[0], fpoint[1]);
-
-        var mapOptions = {
-            'zoom': 17, // initialize zoom level - the max value is 21
-            'streetViewControl': false, // hide the yellow Street View pegman
-            'scaleControl': true, // allow users to zoom the Google Map
-            'mapTypeId': google.maps.MapTypeId.ROADMAP,
-            'center': focuse,
-            'scrollwheel': false
-        };
-
-        map = new google.maps.Map(document.getElementById('googlemaps'), mapOptions);
-
-        // Show the default red marker at the location
-        marker = new google.maps.Marker({
-            'position': latLng,
-            'map': map,
-            'draggable': false,
-            'animation': google.maps.Animation.DROP
-        });
-    }
-
-    google.maps.event.addDomListener(window, 'load', showGoogleMaps);
-});
-
-// Lightbox: for Slick Slider
+/** Lightbox: for Slick Slider                       **/
 $(document).ready(function() {
     var $lightbox = $('#lightbox');
 
